a1e3d67dc4d453f26395bfe572b08b13682993e4
Neel Singh <90748055+dyrok@users.noreply.github.com>
2023-03-20 02:52:13 +0530

    sound-scripts
    okay so this script i wrote at 2am because my mom was reading hanuman chalisa 7 times and i had an idea what if i had a program which i could use to play it for her 7 times... so i realised i can do it using python so i created this... Literally out of no where i woke up. turned on my pc and started typing. this program will play a sound n number of times (n is specified by the User) and shutdown the pc, i had a partial knowledge of using os module and winsound in python so i used them after that i added the Error Elimination using try and except. an great example how python would be helpfulin dad to day life

---
+++ sound-scripts.py
@@ -0,0 +1,24 @@
+import winsound
+import os
+
+print("################################SCRIPT BY NEEL SINGH################################")
+
+file_path = r"specify\the\file\here\must-be.wav"
+try:
+    n = int(input("No of Times you want to play: "))  # Number of times to play the sound
+except NameError and ValueError:
+    print("Please enter an Number")
+    exit()
+
+try:
+    for i in range(n):
+        print("Played This Sound:", i+1,"Times")
+        winsound.PlaySound(file_path, winsound.SND_LOOP)
+
+except NameError and ValueError:
+    print("Please enter an Number")
+    exit()
+
+print("################################SCRIPT BY NEEL SINGH################################")
+
+os.system("shutdown -h")
